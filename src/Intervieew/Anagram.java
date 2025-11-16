9a3816f497bbb6df0a830d59aaa41daf3ac02b1a
prasanta-123 <prasantamallik195@gmail.com>
2025-11-16 23:33:31 +0530

    anagram

--- src/Intervieew/Anagram.java
+++ src/Intervieew/Anagram.java
@@ -1,11 +1,18 @@
 package Intervieew;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Anagram {
     public static void main(String[] args) {
-        String  str1="cat";
-        String str2="act";
+//        String  str1="cat";
+//        String str2="act";
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter first character");
+        String str1 = sc.nextLine();
+        System.out.println("enter second  character");
+
+        String str2 = sc.nextLine();
      char a []  = str1.toCharArray();
      char b []  = str2.toCharArray();
      Arrays.sort(a);
